9e9bf4a486e3e595c1eac80d0de67ac2f319a642
DaBiggestBozo <146139125+DaBiggestBozo@users.noreply.github.com>
2024-09-17 13:50:35 -0700

    debugging wrist

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Wrist.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Wrist.kt
@@ -28,6 +28,9 @@ object Wrist {
     private fun updatePosition(targetPosition: Int){
         wrist.position = targetPosition.toDouble()/270
         state = targetPosition.toString()
+        opmode.telemetry.addData("Wrist State", state)
+        opmode.telemetry.addData("Wrist POSITION", wrist.position)
+        opmode.telemetry.addData("workingC" 5)
     }
 
     private fun changePosition(direction: String){
@@ -36,30 +39,29 @@ object Wrist {
         } else {
             if (direction == "forward" && currentPos != positions[0]) {
                 currentPos == positions[positions.indexOf(currentPos)-1]
+                opmode.telemetry.addData("workingB" 4)
             }
             if (direction == "backward" && currentPos != positions[positions.size-1]) {
                 currentPos == positions[positions.indexOf(currentPos)+1]
+                opmode.telemetry.addData("workingB" 5)
             }
         }
         updatePosition(currentPos)
     }
 
     fun updateWrist() {
-        opmode.telemetry.addData("Wrist State", state)
-        opmode.telemetry.addData("Wrist POSITION", wrist.position)
-
-
         // Check the status of the claw button on the game pad
-        forwardWristButtonCurrentlyPressed = opmode.gamepad1.right_bumper //change this to change the button
-        backwardWristButtonCurrentlyPressed = opmode.gamepad1.left_bumper //change this to change the button
-
+        forwardWristButtonCurrentlyPressed = opmode.gamepad1.right_bumper //change these to change the button
+        backwardWristButtonCurrentlyPressed = opmode.gamepad1.left_bumper
 
         if (!(forwardWristButtonCurrentlyPressed && backwardWristButtonCurrentlyPressed)) { //safety mechanism
             if (forwardWristButtonCurrentlyPressed && !forwardWristButtonPreviouslyPressed) {
                 changePosition("forward")
+                opmode.telemetry.addData("workingA", 1)
             }
             if (backwardWristButtonCurrentlyPressed && !backwardWristButtonPreviouslyPressed) {
                 changePosition("backward")
+                opmode.telemetry.addData("workingA", 2)
             }
         }
 
