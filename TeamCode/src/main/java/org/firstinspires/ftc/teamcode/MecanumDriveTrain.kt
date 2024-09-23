1909b8e833d96d50748e05d95c0debb6e22bb36c
DaBiggestBozo <146139125+DaBiggestBozo@users.noreply.github.com>
2024-09-23 12:33:54 -0700

    made slowmode work FINALLLY

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveTrain.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveTrain.kt
@@ -11,8 +11,8 @@ object MecanumDriveTrain { //Prefix for commands
     private lateinit var rightRear: DcMotor
     private lateinit var rightFront: DcMotor
     lateinit var opmode: OpMode //opmode var innit
-    var currentSpeedDivider = 1.0 //for slow mode
-    var debug = 0
+    private val speedDivider = 3.0 //divider for slow mode
+    var slowMode = false
     private var slowModeButtonCurrentlyPressed = false
     private var slowModeButtonPreviouslyPressed = false
     fun initDrive(opmode: OpMode){ //init motors
@@ -26,7 +26,7 @@ object MecanumDriveTrain { //Prefix for commands
         rightRear.direction = (DcMotorSimple.Direction.FORWARD)
         rightFront.direction = (DcMotorSimple.Direction.FORWARD)
 
-        currentSpeedDivider = 1.0
+        slowMode = false
 
         this.opmode = opmode
     }
@@ -37,42 +37,38 @@ object MecanumDriveTrain { //Prefix for commands
         var rightFrontPower: Double
 
         val strafe = opmode.gamepad1.left_stick_x.toDouble() //can change controls
-        val drive = -opmode.gamepad1.left_stick_y.toDouble()
-        val turn = opmode.gamepad1.right_stick_x.toDouble()
+        val drive = -opmode.gamepad1.left_stick_y.toDouble() // ''
+        val turn = opmode.gamepad1.right_stick_x.toDouble() // ''
         leftBackPower = Range.clip(drive + turn - strafe, -1.0, 1.0)
         leftFrontPower = Range.clip(drive + turn + strafe, -1.0, 1.0)
         rightBackPower = Range.clip(drive - turn + strafe, -1.0, 1.0)
         rightFrontPower = Range.clip(drive - turn - strafe, -1.0, 1.0)
 
-        updateSpeed(3.0) //set to the slowmode divider
+        updateSpeed()
 
-        leftRear.power = leftBackPower/currentSpeedDivider
-        leftFront.power = leftFrontPower/currentSpeedDivider
-        rightRear.power = rightBackPower/currentSpeedDivider
-        rightFront.power = rightFrontPower/currentSpeedDivider
+        if (slowMode) {
+            leftRear.power = leftBackPower/speedDivider
+            leftFront.power = leftFrontPower/speedDivider
+            rightRear.power = rightBackPower/speedDivider
+            rightFront.power = rightFrontPower/speedDivider
+        } else {
+            leftRear.power = leftBackPower
+            leftFront.power = leftFrontPower
+            rightRear.power = rightBackPower
+            rightFront.power = rightFrontPower
+        }
 
         opmode.telemetry.addData("Front Motors", "left (%.2f), right (%.2f)", leftFrontPower, rightFrontPower)
         opmode.telemetry.addData("Back Motors", "left (%.2f), right (%.2f)", leftBackPower, rightBackPower)
-        opmode.telemetry.addData("Divider", "divider (%.2f)", currentSpeedDivider)
-        opmode.telemetry.addData("debug test", debug)
+        opmode.telemetry.addData("Slow mode?", slowMode)
     }
-    private fun updateSpeed(speedDivider: Double){
+    private fun updateSpeed(){
         // Check the status of the speed button on the gamepad
         slowModeButtonCurrentlyPressed = (opmode.gamepad1.right_trigger.toDouble() > 0.1)//change this to change the button
-
         // If the button was pressed
         if (slowModeButtonCurrentlyPressed && !slowModeButtonPreviouslyPressed) {
-            swapSpeed(speedDivider)
-            debug = 1
+            slowMode = !slowMode
         }
         slowModeButtonPreviouslyPressed = slowModeButtonCurrentlyPressed
-    }
-
-    private fun swapSpeed(speedDividerInner: Double) {
-        if (currentSpeedDivider == speedDividerInner) {
-            currentSpeedDivider == 1.0
-        } else {
-            currentSpeedDivider == speedDividerInner
-        }
     }
 }
